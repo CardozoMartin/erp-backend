d18f8b8a31d40232a8046f7e92eb4380f6c43361
Martin Cardozo <martin.cardozo@zymo.it>
2026-05-23 00:51:15 -0300

    se agrega servicio de roles

--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -850,9 +850,7 @@ export class ProductoService {
     let stock = await this.stockRepo.findOne({
       where: {
         producto_id: id,
-        variante_id:
-          dto.variante_id == null ? IsNull() : dto.variante_id,
-        sucursal_id: sucursalId == null ? IsNull() : sucursalId,
+        variante_id:ucursal_id: sucursalId == null ? IsNull() : sucursalId,
       },
     });
     if (!stock) {
--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+ 
+ 
+ 
+ 
+ 
+ 
+ ,
+
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
@@ -24,11 +40,11 @@ export class RolesController {
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
+    return this.rolesService.update(id, updateRoleDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+    return this.rolesService.remove(id);
   }
 }
